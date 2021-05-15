e453b222c282a17b2c799e307a5ceff02e87028b
yugmani <yugmani@yahoo.com>
2021-05-15 00:08:54 -0700

    convert a set into an array

--- script.js
+++ script.js
@@ -18,3 +18,41 @@ function isValuePresent(mySet, item) {
 // Expected true
 // console.log(isValuePresent(new Set('123'), '2'));
 // Expected true
+
+// ********************************************************
+// 2. Convert a Set to Array
+// ********************************************************
+
+// Write a function that takes a Set as argument
+// Convert the Set to an Array
+// Return the Array
+
+function setToArray(mySet) {
+  return [...mySet];
+}
+
+// Test Cases:
+console.log(setToArray(new Set([1, 2, 3])));
+// Expected [1, 2, 3]
+console.log(setToArray(new Set([123])));
+// Expected [123]
+console.log(setToArray(new Set(['1', '2', '3'])));
+// Expected ['1', '2', '3']
+console.log(setToArray(new Set('123')));
+// Expected ['1', '2', '3']
+
+// ********************************************************
+//1. Check if value is present in Set
+// ********************************************************
+
+// ********************************************************
+//1. Check if value is present in Set
+// ********************************************************
+
+// ********************************************************
+//1. Check if value is present in Set
+// ********************************************************
+
+// ********************************************************
+//1. Check if value is present in Set
+// ********************************************************
