10259b7d3f0ed454e0c541186c869a30a9f7908c
yugmani <yugmani@yahoo.com>
2021-05-14 23:55:46 -0700

    initialize the repository

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello!')
+// ********************************************************
+//1. Check if value is present in Set
+// ********************************************************
+
+// Write a function that takes a Set and a value as arguments
+// Check if the value is present in the Set
+
+function myFunction() {
+  return;
+}
+
+// Test Cases:
+myFunction(new Set([1, 2, 3]), 2);
+// Expected true
+myFunction(new Set([123]), 2);
+// Expected false
+myFunction(new Set(['1', '2', '3']), '2');
+// Expected true
+myFunction(new Set('123'), '2');
+// Expected true
